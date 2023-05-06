6f0069589b70503ad841bca8179f6f6d0bd67967
BhushanBhale27 <bhushan27official@gmail.com>
2023-05-06 16:37:57 +0530

    Till banner transition

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logoDark from "./sabkastorecom-new-removebg-preview.png";
+import logoLight from "./sabkastorecom-light-removebg.png";
+import cartImg from "./cart.png";
+import paymentLogo from "./paymentLogo.png";
+import googleLogo from "./googleLogo.png";
+import githubLogo from "./githubLogo.png";
+
+export { logoDark, logoLight, cartImg, paymentLogo, googleLogo, githubLogo };
---
+++ src/components/Banner.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+
+const Banner = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    "https://amazonproone.vercel.app/static/media/img1.efb3d39101f7ef77d616.jpg",
+    "https://amazonproone.vercel.app/static/media/img2.bc1bdb910ead16c65197.jpg",
+    "https://amazonproone.vercel.app/static/media/img5.aa945e25375bfdee385f.jpg",
+    "https://amazonproone.vercel.app/static/media/img3.c80809bb40bee5c34372.jpg",
+    
+  ];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 3 : (prev) => prev - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 3 ? 0 : (prev) => prev + 1);
+  };
+  return (
+    <div className="w-full h-auto overflow-x-hidden">
+      <div className="W-screen h-[650px] relative">
+        <div
+          style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+          className="w-[400vw] h-full flex transition-transform duration-1000"
+        >
+          <img
+            className="w-screen h-full object-cover"
+            src={data[0]}
+            alt="ImgOne"
+            loading="priority"
+          />
+          <img
+            className="w-screen h-full object-cover"
+            src={data[1]}
+            alt="ImgTwo"
+            loading="priority"
+          />
+          <img
+            className="w-screen h-full object-cover"
+            src={data[2]}
+            alt="ImgThree"
+            loading="priority"
+          />
+          <img
+            className="w-screen h-full object-cover"
+            src={data[3]}
+            alt="ImgFour"
+            loading="priority"
+          />
+        </div>
+        <div className="absolute w-fit left-0 right-0 mx-auto flex gap-8 bottom-44">
+          <div
+            onClick={prevSlide}
+            className="w-14 h-12 border-[1px] border-gray-700 flex items-center justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-500"
+          >
+            <AiOutlineLeft />
+          </div>
+          <div
+            onClick={nextSlide}
+            className="w-14 h-12 border-[1px] border-gray-700 flex items-center justify-center hover:cursor-pointer hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-500"
+          >
+            <AiOutlineRight />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
